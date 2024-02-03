2c69a43324ca20aba6b5e6f5a5d453e47a7264e3
Thomas Rice <ricethomasj@gmail.com>
2024-02-03 10:10:36 -0600

    Appended the associated activity objects onto their respective destination

--- sustainableapi/models/activity.py
+++ sustainableapi/models/activity.py
@@ -4,6 +4,6 @@ from .destination import Destination
 
 class Activity(models.Model):
   description = models.TextField()
-  location = models.ForeignKey(Destination, on_delete=models.CASCADE)
+  location = models.ForeignKey(Destination, on_delete=models.CASCADE, related_name="activities")
   user = models.ForeignKey(User, on_delete=models.CASCADE)
   favorite = models.BooleanField()
