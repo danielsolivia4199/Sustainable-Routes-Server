d83057bc0bc7bc998398a9573d1cec77626bf7db
Thomas Rice <ricethomasj@gmail.com>
2024-02-05 21:29:21 -0600

    Appended tags to activity object

--- sustainableapi/models/activity_tag.py
+++ sustainableapi/models/activity_tag.py
@@ -4,4 +4,4 @@ from .activity import Activity
 
 class ActivityTag(models.Model):
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
-    activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
+    activity = models.ForeignKey(Activity, on_delete=models.CASCADE, related_name='tags')
--- sustainableapi/serializers/activity.py
+++ sustainableapi/serializers/activity.py
@@ -1,13 +1,22 @@
 from rest_framework import serializers
-from sustainableapi.models import Activity
+from sustainableapi.models import Activity, ActivityTag
 from .activity_comment import ActivityCommentSerializer
+
+class ActivityTagSerializer(serializers.ModelSerializer):
+    """JSON serializer for game types
+    """
+    class Meta:
+        model = ActivityTag
+        fields = ('id', 'tag',)
+        depth = 1
 
 class ActivitySerializer(serializers.ModelSerializer):
   """JSON serializer for Activities"""
   
   comments = ActivityCommentSerializer(read_only=True, many=True)
+  tags = ActivityTagSerializer(read_only=True, many=True)
   
   class Meta:
     model = Activity
-    fields = ('id', 'description', 'location', 'user', 'favorite', 'comments')
+    fields = ('id', 'description', 'location', 'user', 'favorite', 'comments', 'tags')
     depth = 1
